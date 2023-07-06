7fab3bad2c887c2c017d6ef89ce468b9673efcd0
Tiago Abreu <t149014@dac.unicamp.br>
2023-07-06 12:27:03 -0300

    Update conan_profile.bash

--- conan_profile.bash
+++ conan_profile.bash
@@ -2,5 +2,6 @@
 
 echo "Creating package profile - conan_profile"
 conan profile new conan_profile --detect
+conan profile update settings.compiler.libcxx=libstdc++11 conan_profile
 conan profile update env.CXX=clang++-9 conan_profile
 conan profile show conan_profile
