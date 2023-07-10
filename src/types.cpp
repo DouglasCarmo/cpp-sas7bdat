51c339826f1a0aa7c86da0c13c5fe2ca881121b4
Tiago <tiago.am94@gmail.com>
2023-07-10 10:34:51 -0300

    COUT date

--- src/types.cpp
+++ src/types.cpp
@@ -35,6 +35,7 @@ std::string to_string(DATETIME _x) {
 std::string to_string(DATE _x) {
   char szBuffer[16];
   size_t size{0};
+  std::cout << _x;
   auto r = fmt::format_to_n(szBuffer, sizeof(szBuffer), "{:04}-{:02}-{:02}",
                             _x.year(), _x.month(), _x.day());
   size = r.size;
