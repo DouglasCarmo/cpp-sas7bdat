4b3d3d3c8f942abaf7d7081d19a2eda2d138aca2
Tiago <tiago.am94@gmail.com>
2023-07-10 11:10:31 -0300

    COUT date

--- src/types.cpp
+++ src/types.cpp
@@ -37,6 +37,9 @@ std::string to_string(DATE _x) {
   size_t size{0};
   auto r = fmt::format_to_n(szBuffer, sizeof(szBuffer), "{:02}/{:02}/{:04}",
                             _x.day(), _x.month(), _x.year());
+  std::ostream &os;
+
+  fmt::print(os, _x);
   size = r.size;
   return std::string(szBuffer, size);
 }
