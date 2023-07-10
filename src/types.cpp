507c4178a0bcd01ba9696ccca6cfccd2c01f3883
Tiago <tiago.am94@gmail.com>
2023-07-10 11:17:27 -0300

    remove cout  date

--- src/types.cpp
+++ src/types.cpp
@@ -37,7 +37,7 @@ std::string to_string(DATE _x) {
   size_t size{0};
   auto r = fmt::format_to_n(szBuffer, sizeof(szBuffer), "{:02}/{:02}/{:04}",
                             _x.day(), _x.month(), _x.year());
-  std::cout << _x;
+  // std::cout << _x;
   size = r.size;
   return std::string(szBuffer, size);
 }
